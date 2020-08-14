38688652a3a27f3242599e4dcba500d2149aad4e
Diego Borigen <borigendiego@gmail.com>
2020-08-14 10:45:17 +1000

    Estetica pagina projects

--- src/components/projects/Gallery.js
+++ src/components/projects/Gallery.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Collapsible from 'react-collapsible';
 import {
     Link
-} from "react-router-dom";
+} from 'react-router-dom';
 //css
 import './gallery.scss';
 
@@ -16,37 +16,53 @@ const Gallery = () => {
     return (
         <div className={'gallery-wrapper'}>
             <div className={'gray-background'} />
+
             <div className={'gallery-container'}>
-                <div className={'images-wrapper'}>
-                    {projects.map((image, index) =>
-                        <div
-                            key={index}
-                            className={'gallery-slide'}
+                <div className={'gallery-overlay-helper'}>
+                    <div className={'images-wrapper'}>
+                        {projects.map((image, index) =>
+                            <div
+                                key={index}
+                                className={'gallery-slide'}
+                            >
+                                <Link to={'/projects'}>
+                                    <img className={'gallery-image'} alt={'as'} src={'http://conradarchitect.com/wp-content/uploads/2014/09/third.jpg'} />
+                                </Link>
+                            </div>
+                        )}
+                    </div>
+                    <Collapsible trigger={
+                        <a
+                            onClick={() => {
+                                setExpandSection(!expandSection)
+                            }}
                         >
-                            <Link to={'/projects'}>
-                                <img className={'gallery-image'} alt={'as'} src={'http://conradarchitect.com/wp-content/uploads/2014/09/third.jpg'} />
-                            </Link>
+                            <h2 className={'all-projects-cta'}>SEE ALL PROJECTS</h2>
+                        </a>
+                    }>
+                        <div className={'images-wrapper'}>
+                            {fullProjects.splice(0, 2).map((image, index) =>
+                                <div
+                                    key={index}
+                                    className={'gallery-slide more-slides'}
+                                >
+                                    <Link to={'/projects'}>
+                                        <img className={'gallery-image'} alt={'as'} src={'http://conradarchitect.com/wp-content/uploads/2014/09/third.jpg'} />
+                                    </Link>
+                                </div>
+                            )}
                         </div>
-                    )}
+                        <a
+                            onClick={() => {
+                                //TODO: Logica para hacer que se cierre la secion
+                                setExpandSection(!expandSection)
+                            }}
+                        >
+                            <h2 className={'all-projects-cta'}>SEE LESS</h2>
+                        </a>
+                    </Collapsible>
                 </div>
-                <Collapsible trigger={
-                    <a
-                        onClick={() => {
-                            setExpandSection(!expandSection)
-                        }}
-                    >
-                        <h2 className={'all-projects-cta'}>SEE ALL PROJECTS</h2>
-                    </a>
-                }>
-                    {fullProjects.splice(0, 2).map((image, index) =>
-                        <div
-                            key={index}
-                            className={'gallery-slide more-slides'}
-                        >
-                            <img alt={'as'} src={'http://conradarchitect.com/wp-content/uploads/2014/09/third.jpg'} />
-                        </div>
-                    )}
-                </Collapsible>
+
             </div>
         </div>
     )
