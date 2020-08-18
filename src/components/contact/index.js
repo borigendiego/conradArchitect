740cdadb0f3fa19cc7f0499887452519013a17b4
Diego Borigen <borigendiego@gmail.com>
2020-08-18 22:31:13 +1000

    Refactor on contactSection

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import './contacs.scss';
+import SectionLayout from '../common/SectionLayout';
 
 const Contact = () => {
     return (
-        <>
-            <h1 className={'section-title'}>CONTACT</h1>
-            <div className={'contact-container'}>
-                <div className={'large-section-container contact-black-border'} />
-                <div className={'contact-content'}>
-                    <p>mail@mail.com</p>
-                    <p>(000) 123456789</p>
-                    <p>Test test test test</p>
-                    <p>Test test test test</p>
-                </div>
+        <SectionLayout sectionTitle={'CONTACT'}>
+            <div className={'contact-content'}>
+                <p>mail@mail.com</p>
+                <p>(000) 123456789</p>
+                <p>Test test test test</p>
+                <p>Test test test test</p>
             </div>
-        </>
+        </SectionLayout>
     )
 };
 
