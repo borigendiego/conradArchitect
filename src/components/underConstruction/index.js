f419f04c57cd7cca633f95ed639adc019f0c57cb
Diego Borigen <borigendiego@gmail.com>
2020-12-17 15:40:39 +1100

    New under construction page

--- src/components/underConstruction/index.js
+++ src/components/underConstruction/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Crane from '../../assets/icons/crane-color.svg';
+import ConstructionIcon from '../../assets/icons/construction-icon.png';
 //Style
 import './underConstruction.scss'
 
@@ -7,11 +8,13 @@ const UnderConstructionPage = () => {
     return (
         <div className={'uc_container'}>
             <div className={'uc_content_wrapper'}>
-                <img alt={'underConstruction'} src={Crane} />
-                <h1>404</h1>
-                <h4>Site under construction</h4>
-                <p>You can reach us here:</p>
-                <p>jconradarc@gmail.com</p>
+                <img alt={'underConstruction'} src={ConstructionIcon} />
+                <div className={'text-container'}>
+                    <h1>404</h1>
+                    <h4>Site under construction</h4>
+                    <p>You can reach us here:</p>
+                    <p>jconradarc@gmail.com</p>
+                </div>
             </div>
         </div>
     )
