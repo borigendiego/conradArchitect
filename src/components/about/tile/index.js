d6edc40f15ccc4de5e8d9f3683623370900f09b7
Diego Borigen <borigendiego@gmail.com>
2020-12-22 15:14:04 +1100

    Tile refactor

--- src/components/about/tile/index.js
+++ src/components/about/tile/index.js
@@ -1,21 +1,17 @@
 import React from 'react';
-//Styles
-import './tile.scss';
 
 const Tile = (props) => {
     const { key, name, lastName, rol, description, profilePicture } = props.staffData;
 
     return (
-        <div className={'tile-wrapper'}>
-            <div className={'tide-content-wrapper'}>
-                <img alt={`${key}-profile`} src={profilePicture} />
-                <div className={'text-wrapper'}>
-                    <h4 className={'tile-name-text'}>{`${name} ${lastName}`}</h4>
-                    <h4 className={'tile-text'}>{rol}</h4>
-                    <h4 className={'tile-text'}>{description}</h4>
-                </div>
-                <a href={'#home'} className={'tile-full-profile'}>FULL PROFILE ></a>
+        <div className={'team-tile member-tile'}>
+            <img alt={`${key}-profile`} src={profilePicture} />
+            <div className={'text-wrapper'}>
+                <h5 className={'tile-name-text'}>{`${name} ${lastName}`}</h5>
+                <h6 className={'tile-text'}>{rol}</h6>
+                <h6 className={'tile-text'}>{description}</h6>
             </div>
+            <a href={'#home'} className={'tile-link'}>FULL PROFILE ></a>
         </div>
     )
 };
