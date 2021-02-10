419bce57bbe89855281d7cee62d5c037e4c18c5c
Juan <jborigen9@gmail.com>
2021-02-10 13:15:27 -0300

    Team text changes, profile tile space change

---
+++ src/components/about/constants.js
@@ -0,0 +1,15 @@
+const ABOUT_PARAGRAPH_ONE = 'The Architectural firm, James Conrad, Architects (JCA )' +
+    ' was founded in 1990 by James Conrad. James concurrently found a development company called Conrad Development.';
+
+const ABOUT_PARAGRAPH_TWO = 'JCA provide highly personalized service. Our wealth of experience is invaluable in' +
+    ' selecting the right property. We can provide guidance through all phases of building a home starting at' +
+    ' assistance with finding the right property';
+
+const ABOUT_PARAGRAPH_THREE = 'JCA works primarily in Southern California but has projects throughout the' +
+    ' United States, Canada and Mexico.';
+
+const ABOUT_PARAGRAPH_FOUR = 'The combination of experience and highly personalized service allows JCA provide' +
+    ' a full array of services to clients wanting to build their dream home.';
+
+
+export { ABOUT_PARAGRAPH_ONE, ABOUT_PARAGRAPH_TWO, ABOUT_PARAGRAPH_THREE, ABOUT_PARAGRAPH_FOUR };
--- src/components/about/index.js
+++ src/components/about/index.js
@@ -5,7 +5,9 @@ import './about.scss';
 //Components
 import SectionLayout from '../common/SectionLayout';
 import Tile from './tile';
-//import {Link} from "react-router-dom";
+//Constant
+import './constants';
+import {ABOUT_PARAGRAPH_FOUR, ABOUT_PARAGRAPH_ONE, ABOUT_PARAGRAPH_THREE, ABOUT_PARAGRAPH_TWO} from "./constants";
 
 const About = () => {
     //TODO: Finish with the API implementation.
@@ -52,30 +54,17 @@ const About = () => {
                     />
                     <div className={'teams-text-wrapper'}>
                         <h2 className={'tile-name-text'}>CONRAD ARCHITECTS</h2>
-                        <p>
-                            The Architectural firm, James Conrad, Architects (JCA ) was founded in 1990 by James Conrad.
-                            James concurrently found a development company called Conrad Development.
-                            JCA, focused it’s energy on designing high-end homes in South Orange County for clients
-                            while the Development company, built residential properties, primarily high-end single-family
-                            residences in Laguna Beach.  The JCA has always been a very small firm with just one or two
-                            full-time employees. The intent was to provide highly personalized service and allow James
-                            to closely oversee all work. The development company was also small and developed or
-                            remodeled just a few homes each year. This unique, this combination of experience allows
-                            JCA to provide a full array of services to clients wanting to build their dream home.
-                            James, and his staff, can provide guidance through all phases of building a home starting
-                            at assistance with finding the right property.  Once a  property is identified JCA can’t
-                            help the client fully assess the property to insure that it is the best one for their needs.
-                            For example, a lot that is available for purchase at a lower price may actually end up
-                            costing much more to develop due to foundation and other issues.  In addition, some lots
-                            have greater potential for development opportunities and others.  James's wealth of
-                            experience is in valuable in selecting the right property.
-                            Once a property is decided upon,  JCA can help the client refine the program for the
-                            home, design the home,  and procure all necessary entitlements required for building.
-                            JCA can assist the client in bidding the project to qualified contractors and help the
-                            client decide which builder is the best fit for them.  JCA can assist with financing the
-                            project and assist the client with all aspects of construction. JCA works primarily in
-                            Southern California but has projects throughout the United States, Canada and Mexico.
-                        </p>
+                        <div className={'about-text-wrapper'}>
+                            <div className={'about-paragraphs'}>
+                                <p>{ABOUT_PARAGRAPH_ONE}</p>
+                                <p>{ABOUT_PARAGRAPH_TWO}</p>
+                                <p>{ABOUT_PARAGRAPH_THREE}</p>
+                            </div>
+                            <div className={'about-quote'}>
+                                <p className={'quote-text'}>&#8220;{ABOUT_PARAGRAPH_FOUR}&#8221;</p>
+                                <span className={'quote-name'}>-James Conrad, President</span>
+                            </div>
+                        </div>
                     </div>
                    {/* <Link to={'/'} className={'tile-link team-link'}>
                         FULL PAGE >
