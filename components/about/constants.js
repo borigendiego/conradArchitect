cd3de71030015b2121b487d680c4dd4d3d80914d
Diego Borigen <diego@iddigital.com.au>
2021-09-22 10:57:57 +1000

    Fix typo in About text

--- components/about/constants.js
+++ components/about/constants.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ABOUT_TEXT = [
-    'The Architectural firm, James Conrad, Architects (JCA ) was founded in 1990 by James Conrad. James concurrently found a development company called Conrad Development.',
+    'The Architectural firm, James Conrad, Architects (JCA) was founded in 1990 by James Conrad. James concurrently found a development company called Conrad Development.',
     'JCA provide highly personalized service. Our wealth of experience is invaluable in selecting the right property. We can provide guidance through all phases of building a home starting at assistance with finding the right property',
     'JCA works primarily in Southern California but has projects throughout the United States, Canada and Mexico.',
     'The combination of experience and highly personalized service allows JCA provide a full array of services to clients wanting to build their dream home.',
